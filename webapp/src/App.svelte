69b1ae5e52ffdf5113aed8bc8fb774627c2d00c4
Roy C. Davies <roycdavies@gmail.com>
2024-12-14 07:57:40 +1300

    First push

---
+++ webapp/src/App.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import {Button, Menu, Container, Link, Item, Segment, Header, Table, Table_Body, Table_Col, Table_Row, Icon, Message, Content, Column, Image, Labels, Label } from 'svelte-fomantic-ui';
+
+    import MainMenu from "./lib/MainMenu.svelte";
+
+    import Start from "./lib/Start.svelte";
+    import About from "./lib/About.svelte";
+    import StrongIdeas from "./lib/StrongIdeas.svelte";
+    import QuadrupleDiamond from "./lib/QuadrupleDiamond.svelte";
+    import Code from "./lib/Code.svelte";
+    import Contact from "./lib/Contact.svelte";
+
+    let page = "start";
+</script>
+
+<main>
+    <Segment ui inverted vertical center aligned style="min-height:1000px">
+
+        <MainMenu bind:page/>
+
+        {#if page==="start"}
+            <Start {page}/>
+        {:else if page==="about"}
+            <About {page}/>
+        {:else if page==="strong_ideas"}
+            <StrongIdeas {page}/>
+        {:else if page==="quadruple"}
+            <QuadrupleDiamond {page}/>
+        {:else if page==="code"}
+            <Code {page}/>
+        {:else if page==="contact"}
+            <Contact {page}/>
+        {/if}
+    </Segment>
+
+</main>
+
+<style>
+
+</style>
---
+++ webapp/src/lib/Start.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { Container, Header, Image } from "svelte-fomantic-ui";
+
+    import reality2bee from "../assets/reality2 enabled.png"
+
+    export let page = "start";
+    
+</script>
+
+<Container ui style="padding-top:50px;min-height:200px;padding-bottom:50px;">
+    <Header ui h2 inverted>Welcome to Reality2.</Header>
+    <Header ui h3 inverted>A new framework for truly assistive technologies.</Header>
+    {#if page==="start"}
+        <Image ui src={reality2bee}/>
+    {/if}
+</Container>
