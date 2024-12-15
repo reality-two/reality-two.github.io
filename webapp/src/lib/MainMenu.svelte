93559110c5d3e100247ae012a7bc3580b5d69b30
Roy C. Davies <roycdavies@gmail.com>
2024-12-16 12:22:24 +1300

    Updates

--- webapp/src/lib/MainMenu.svelte
+++ webapp/src/lib/MainMenu.svelte
@@ -8,7 +8,7 @@
     <Menu ui large secondary inverted pointing stackable>
         <Link item active={page==="start"} blue={page==="start"} on:click={()=>page="start"}>Home</Link>
         <Link item active={page==="objectsandactors"} blue={page==="objectsandactors"} on:click={()=>page="objectsandactors"}>Objects and Actors</Link>
-        <!-- <Link item active={page==="sentants"} blue={page==="sentants"} on:click={()=>page="sentants"}>Sentient Agents</Link> -->
+        <Link item active={page==="sentants"} blue={page==="sentants"} on:click={()=>page="sentants"}>Sentient Agents</Link>
         <Link item active={page==="transientnetworks"} blue={page==="transientnetworks"} on:click={()=>page="transientnetworks"}>Transient Networks</Link>
         <Menu right>
             <Link item active={page==="information"} green={page==="information"} on:click={()=>page="information"}>Information</Link>
--- webapp/src/lib/Sentants.svelte
+++ webapp/src/lib/Sentants.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { Container, Column, Text, Divider } from "svelte-fomantic-ui";
+    import Link from "svelte-fomantic-ui/src/lib/elements/Link.svelte";
 
     export let page = "start";
 </script>
@@ -15,23 +16,23 @@
         </Text>
         <Divider ui/>
         <Text ui large>
-            zero knowledge
+            We also call Sentants 'Bees', that can work together in 'Swarms'.  Sentants can be programmed (given 'Behaviours') and have their functionality extended using 'Plugins' or 'Antennae' using the bee metaphor.
         </Text>
         <Divider ui/>
         <Text ui large>
-            local first data policy
+            A key guideline is that data is to be stored close to its source (with encrypted backups elsewhere as required).  This means that your data is quite literally 'on you', in the devices you wear and carry.  But don't worry, if you lose them, others won't be able to access your personal information.
         </Text>
         <Divider ui/>
         <Text ui large>
-            emergant behaviours
+            Each Sentant (or Bee) has a range of properties, namely it is: Programmeable, Immutable (can't be changed once created), Persistant (exists and 'runs' until it is removed), Unique (only one in the world) and Consistent (it is perceived the same regardless of the visualiser).
         </Text>
         <Divider ui/>
         <Text ui large>
-            programmeable, immutable, persistant, unique, consistent
+            Sentants / Bees are set up through loading a script to a Reality2 Node.  These scripts can be exchanged with others to <Link href="https://github.com/reality-two/reality2-definitions" target="_blank">share functionality</Link>, and even build up whole libraries of capability.
         </Text>
         <Divider ui/>
         <Text ui large>
-            Scriptable
+            Swarms of Sentants on Reality2 nodes have the emergent property of the 'Objects and Actors' metaphor.
         </Text>
         <Divider ui/>
     </Column>
