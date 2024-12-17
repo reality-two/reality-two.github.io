5793aadb3ff080422bee34a2b8ec38f3538618b6
Roy C. Davies <roycdavies@gmail.com>
2024-12-18 08:18:28 +1300

    Tweaks to slidedeck UI

--- webapp/src/lib/Slidedeck.svelte
+++ webapp/src/lib/Slidedeck.svelte
@@ -18,18 +18,30 @@
 
     let counter = 0;
 
+    let width;
+
+    // Bind the window's innerWidth to the width variable
+    $: window.addEventListener('resize', () => {
+        width = window.innerWidth;
+    });
+
+    // Set initial width
+    width = window.innerWidth;
+
 </script>
 
 <Container ui style="padding-top:20px;min-height:200px;padding-bottom:0px;">
     <Column style="padding-bottom: 0px;">
         <div style="padding: 0px; position: relative;">
             <Image ui fluid src={images[counter]}/>
-            <Button ui icon popup massive basic disabled={counter == 0} data-tooltip="Previous" data-position="top left" style="position: absolute; top: 50%; left: 10px; transform: translateY(-50%);" on:click={() => counter = Math.max(0, (counter - 1))}>
-                <Icon angle left grey/>
-            </Button>
-            <Button ui icon popup massive basic disabled={counter == (images.length-1)} data-tooltip="Next" data-position="top right" style="position: absolute; top: 50%; right: 10px; transform: translateY(-50%);" on:click={() => counter = Math.min(images.length-1, (counter + 1))}>
-                <Icon angle right grey/>
-            </Button>
+            {#if width > 600}
+                <Button ui icon popup massive basic disabled={counter == 0} data-tooltip="Previous" data-position="top left" style="position: absolute; top: 50%; left: 10px; transform: translateY(-50%);" on:click={() => counter = Math.max(0, (counter - 1))}>
+                    <Icon angle left grey/>
+                </Button>
+                <Button ui icon popup massive basic disabled={counter == (images.length-1)} data-tooltip="Next" data-position="top right" style="position: absolute; top: 50%; right: 10px; transform: translateY(-50%);" on:click={() => counter = Math.min(images.length-1, (counter + 1))}>
+                    <Icon angle right grey/>
+                </Button>
+            {/if}
         </div>
     </Column>
     <Column center aligned>
