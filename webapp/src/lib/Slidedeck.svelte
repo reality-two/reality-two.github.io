1c6d526225afc30e5b50c4f42e6ce1fb02e8cf99
Roy C. Davies <roycdavies@gmail.com>
2024-12-18 07:45:27 +1300

    Tweaks to slidedeck UI

--- webapp/src/lib/Slidedeck.svelte
+++ webapp/src/lib/Slidedeck.svelte
@@ -22,17 +22,23 @@
 
 <Container ui style="padding-top:20px;min-height:200px;padding-bottom:0px;">
     <Column style="padding-bottom: 0px;">
-        <Button ui image on:click={() => counter = Math.min(images.length-1, (counter + 1))} style="padding: 0px;">
+        <div style="padding: 0px; position: relative;">
             <Image ui fluid src={images[counter]}/>
-        </Button>
+            <Button ui icon popup massive basic disabled={counter == 0} data-tooltip="Previous" data-position="top left" style="position: absolute; top: 50%; left: 10px; transform: translateY(-50%);" on:click={() => counter = Math.max(0, (counter - 1))}>
+                <Icon angle left grey/>
+            </Button>
+            <Button ui icon popup massive basic disabled={counter == (images.length-1)} data-tooltip="Next" data-position="top right" style="position: absolute; top: 50%; right: 10px; transform: translateY(-50%);" on:click={() => counter = Math.min(images.length-1, (counter + 1))}>
+                <Icon angle right grey/>
+            </Button>
+        </div>
     </Column>
     <Column center aligned>
         <Buttons ui basic>
-            <Button ui grey icon on:click={() => counter = Math.max(0, (counter - 1))}>
+            <Button ui grey icon disabled={counter == 0} on:click={() => counter = Math.max(0, (counter - 1))}>
                 <Icon angle left grey/>
             </Button>
             <Button ui blue on:click={() => counter = 0}><Text ui grey>{counter + 1} of {images.length}</Text></Button>
-            <Button ui grey icon on:click={() => counter = Math.min(images.length-1, (counter + 1))}>
+            <Button ui grey icon disabled={counter == (images.length-1)} on:click={() => counter = Math.min(images.length-1, (counter + 1))}>
                 <Icon angle right grey/>
             </Button>
         </Buttons>
