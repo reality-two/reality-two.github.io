b7df686e330490d2e8a8be7eecc2d065c8716d6b
Roy C. Davies <roycdavies@gmail.com>
2025-01-15 13:09:41 +1300

    Tweaks

--- webapp/src/lib/Start.svelte
+++ webapp/src/lib/Start.svelte
@@ -11,12 +11,14 @@
     
 </script>
 <Container ui grid stackable style="padding-top:50px;padding-bottom:0px;vertical-align:top;">
-    <Row>
+    <Row style="padding-top:0px;">
         <Column five wide>
             <Image ui src={reality2bee}/>
         </Column>
-        <Column eleven wide justified style="border-top: 1px solid #4B6E8F; border-bottom: 1px solid #4B6E8F; padding-top: 20px; padding-bottom: 20px;">
+        <Column eleven wide justified >
+            <div style="border-top: 0.5px solid rgb(100, 100, 100); margin-left: -3px; padding-top:20px;"></div>
             <Text ui big>Reality2 connects the digital and physical worlds through Mixed Reality and Calm Technology, powered by sentient digital agents (Sentants). These enable intuitive, context-aware interactions with real objects and virtual entities that blend into your environment, enhancing engagement, protecting privacy, and helping you stay present in your surroundings and social interactions.</Text>
+            <div style="border-bottom: 0.5px solid rgb(100, 100, 100); margin-left: -3px; padding-bottom:22px;"></div>
         </Column>
     </Row>
     <Row>
