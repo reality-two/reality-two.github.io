ac19eae1701e40b24c53ab59e2a53d631dc37413
Roy C. Davies <roycdavies@gmail.com>
2025-01-24 06:32:19 +1300

    Formatting

--- webapp/src/lib/Contact.svelte
+++ webapp/src/lib/Contact.svelte
@@ -8,16 +8,18 @@
     <Column sixteen wide centered>
         <Text ui big>Contact</Text>
     </Column>
-    <Table ui inverted centered huge>
-        <Table_Body>
-            <Table_Row>
-                <Table_Col right aligned>Github <Icon ui github/></Table_Col>
-                <Table_Col><Link href="https://github.com/reality-two" target="_blank">https://github.com/reality-two</Link></Table_Col>
-            </Table_Row>
-            <Table_Row>
-                <Table_Col right aligned>Head Developer <Icon ui running/></Table_Col>
-                <Table_Col><Link href="mailto:contact@reality2.ai?subject=Making contact">Dr. Roy C. Davies</Link></Table_Col>
-            </Table_Row>
-        </Table_Body>
-    </Table>
+    <Column sixteen wide justified>
+        <Table ui inverted centered huge>
+            <Table_Body>
+                <Table_Row>
+                    <Table_Col right aligned>Github <Icon ui github/></Table_Col>
+                    <Table_Col><Link href="https://github.com/reality-two" target="_blank">https://github.com/reality-two</Link></Table_Col>
+                </Table_Row>
+                <Table_Row>
+                    <Table_Col right aligned>Head Developer <Icon ui running/></Table_Col>
+                    <Table_Col><Link href="mailto:contact@reality2.ai?subject=Making contact">Dr. Roy C. Davies</Link></Table_Col>
+                </Table_Row>
+            </Table_Body>
+        </Table>
+    </Column>
 </Container>
--- webapp/src/lib/Information.svelte
+++ webapp/src/lib/Information.svelte
@@ -24,46 +24,50 @@
             Below are links to more information, and the code.  Check in often for updates.  Send feedback, note bugs and ask questions through the appropriate channels on the GitHub repositories.
         </Text>
     </Column>
-    <Table ui inverted centered huge>
-        <Table_Body>
-            <Table_Row>
-                <Table_Col right aligned>Documentation <Icon ui splotch/></Table_Col>
-                <Table_Col><Link href="https://github.com/reality-two/reality2-documentation" target="_blank">https://github.com/reality-two/reality2-documentation</Link></Table_Col>
-            </Table_Row>
-            <Table_Row>
-                <Table_Col right aligned>Reality2 Node core <Icon ui microchip/></Table_Col>
-                <Table_Col><Link href="https://github.com/reality-two/reality2-node-core-elixir" target="_blank">https://github.com/reality-two/reality2-node-core-elixir</Link></Table_Col>
-            </Table_Row>
-            <Table_Row>
-                <Table_Col right aligned>Sentant and Swarm definitions <Icon ui book/></Table_Col>
-                <Table_Col><Link href="https://github.com/reality-two/reality2-definitions" target="_blank">https://github.com/reality-two/reality2-definitions</Link></Table_Col>
-            </Table_Row>
-            <Table_Row>
-                <Table_Col right aligned>Godot VR Client <Icon ui glasses/></Table_Col>
-                <Table_Col><Link href="https://github.com/reality-two/reality2-godot-client" target="_blank">https://github.com/reality-two/reality2-godot-client</Link></Table_Col>
-            </Table_Row>
-            <Table_Row>
-                <Table_Col right aligned>IOT WebApp Demo <Icon ui gamepad/></Table_Col>
-                <Table_Col><Link href="https://github.com/reality-two/reality2-webapp-iotdemo" target="_blank">https://github.com/reality-two/reality2-webapp-iotdemo</Link></Table_Col>
-            </Table_Row>
-            <Table_Row>
-                <Table_Col right aligned>Default Sentants WebApp <Icon ui gamepad/></Table_Col>
-                <Table_Col><Link href="https://github.com/reality-two/reality2-node-core-elixir/tree/main/web/sentants" target="_blank">https://github.com/reality-two/reality2-node-core-elixir/tree/main/web/sentants</Link></Table_Col>
-            </Table_Row>
-        </Table_Body>
-    </Table>
+    <Column sixteen wide justified>
+        <Table ui inverted centered huge>
+            <Table_Body>
+                <Table_Row>
+                    <Table_Col right aligned>Documentation <Icon ui splotch/></Table_Col>
+                    <Table_Col><Link href="https://github.com/reality-two/reality2-documentation" target="_blank">https://github.com/reality-two/reality2-documentation</Link></Table_Col>
+                </Table_Row>
+                <Table_Row>
+                    <Table_Col right aligned>Reality2 Node core <Icon ui microchip/></Table_Col>
+                    <Table_Col><Link href="https://github.com/reality-two/reality2-node-core-elixir" target="_blank">https://github.com/reality-two/reality2-node-core-elixir</Link></Table_Col>
+                </Table_Row>
+                <Table_Row>
+                    <Table_Col right aligned>Sentant and Swarm definitions <Icon ui book/></Table_Col>
+                    <Table_Col><Link href="https://github.com/reality-two/reality2-definitions" target="_blank">https://github.com/reality-two/reality2-definitions</Link></Table_Col>
+                </Table_Row>
+                <Table_Row>
+                    <Table_Col right aligned>Godot VR Client <Icon ui glasses/></Table_Col>
+                    <Table_Col><Link href="https://github.com/reality-two/reality2-godot-client" target="_blank">https://github.com/reality-two/reality2-godot-client</Link></Table_Col>
+                </Table_Row>
+                <Table_Row>
+                    <Table_Col right aligned>IOT WebApp Demo <Icon ui gamepad/></Table_Col>
+                    <Table_Col><Link href="https://github.com/reality-two/reality2-webapp-iotdemo" target="_blank">https://github.com/reality-two/reality2-webapp-iotdemo</Link></Table_Col>
+                </Table_Row>
+                <Table_Row>
+                    <Table_Col right aligned>Default Sentants WebApp <Icon ui gamepad/></Table_Col>
+                    <Table_Col><Link href="https://github.com/reality-two/reality2-node-core-elixir/tree/main/web/sentants" target="_blank">https://github.com/reality-two/reality2-node-core-elixir/tree/main/web/sentants</Link></Table_Col>
+                </Table_Row>
+            </Table_Body>
+        </Table>
+    </Column>
     <Column sixteen wide justified>
         <Divider ui/>
         <Text ui large>
             If you are curious about how this website was created, it is a static site created using Svelte, with the graphics library Fomantic UI using the vite framework.  It is hosted on github.io.
         </Text>
     </Column>
-    <Table ui inverted centered huge>
-        <Table_Body>
-            <Table_Row>
-                <Table_Col right aligned>Reality2 Website <Icon ui globe/></Table_Col>
-                <Table_Col><Link href="https://github.com/reality-two/reality-two.github.io" target="_blank">https://github.com/reality-two/reality-two.github.io</Link></Table_Col>
-            </Table_Row>
-        </Table_Body>
-    </Table>
+    <Column sixteen wide justified>
+        <Table ui inverted centered huge>
+            <Table_Body>
+                <Table_Row>
+                    <Table_Col right aligned>Reality2 Website <Icon ui globe/></Table_Col>
+                    <Table_Col><Link href="https://github.com/reality-two/reality-two.github.io" target="_blank">https://github.com/reality-two/reality-two.github.io</Link></Table_Col>
+                </Table_Row>
+            </Table_Body>
+        </Table>
+    </Column>
 </Container>
